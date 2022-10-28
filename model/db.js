3d61628a22c50869f0e0032fd64f0143c392aa32
benrobo <alumonabenaiah71@gmail.com>
2022-10-28 05:14:13 -0700

    updated major changes

--- model/db.js
+++ model/db.js
@@ -1,20 +1,7 @@
 import dotenv from "dotenv"
 import mysql from "mysql"
-import pg from "pg"
-
-const { Pool } = pg
 
 dotenv.config()
-
-// postgresql connection
-
-// export const conn = new Pool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PWD,
-// })
-
 // SingleStore x MySql connection
 
 export const conn = mysql.createConnection({
