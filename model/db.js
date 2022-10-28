44149c335fbf6bfa737996be91945dbc30bd81cd
benrobo <alumonabenaiah71@gmail.com>
2022-10-28 01:02:14 -0700

    updated

--- model/db.js
+++ model/db.js
@@ -18,7 +18,7 @@ dotenv.config()
 // SingleStore x MySql connection
 
 export const conn = mysql.createConnection({
-    host: 'svc-768864ab-698c-462a-8989-47504edbad71-dml.aws-virginia-4.svc.singlestore.com',
+    host: 'svc-4984488b-31cb-4ad5-8b26-c972ca23018d-dml.aws-virginia-4.svc.singlestore.com',
     user: 'admin',
     password: "Benrobo-tut71",
     database: "eflow"
